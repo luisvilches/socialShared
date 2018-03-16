c4876c039fb89e8dfd25af0d9265d9e709425d68
Cardumen <cardumen@luisvilches.local>
2018-03-16 17:07:04 -0300

    linkedin

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 class SocialShare{
     constructor(obj){
-        this.AppIDfb = obj.facebook.appId;
         this.facebook();
         this.twitter();
+        this.linkedin();
     }
 
     facebook(){
@@ -51,7 +51,7 @@ class SocialShare{
         btnTw.forEach(function(item,index){
             item.addEventListener("click", function(e){
                 e.preventDefault();
-                var arr = ["https://twitter.com/intent/tweet?"];//text=
+                var arr = ["https://twitter.com/intent/tweet?"];
 
                 var url;var width;var height;var text;var via;var hashtag;
 
@@ -97,22 +97,69 @@ class SocialShare{
         })
     }
 
-    test(){
-        //var arr = ["http://luisvilches.cl?","hashtags=juanito,perez","url=http://google.cl","via=luisvilche"];
 
-        //console.log(arr.join('&'));
+    linkedin(){
+        var btnTw = document.querySelectorAll("[social=linkedin]");
+        btnTw.forEach(function(item,index){
+            item.addEventListener("click", function(e){
+                e.preventDefault();
+                var arr = ["https://www.linkedin.com/shareArticle?mini=true"];
 
-        var twitterWindow = window.open('http://facebook.com/sharer.php?u=http://google.cl', '', 'height=600,width=600');
+                var url;var width;var height;var text;var via;var hashtag;var title;
+
+                if( item.dataset.text !== "" && item.dataset.text != null && item.dataset.text != undefined){
+                    text = "text="+item.dataset.text;
+                    arr.push(text);
+                }
+
+                if( item.dataset.title !== "" && item.dataset.title != null && item.dataset.title != undefined){
+                    title = "title="+item.dataset.title;
+                    arr.push(title);
+                }
+
+                if( item.dataset.url === "" ||  item.dataset.url == null ||  item.dataset.url == undefined){
+                    url = "url="+window.location.href;
+                    arr.push(url);
+                } else {
+                    url = "url="+item.dataset.url;
+                    arr.push(url);
+                }
+
+                if( item.dataset.summary !== "" && item.dataset.summary != null && item.dataset.summary != undefined){
+                    via = "summary="+item.dataset.summary;
+                    arr.push(via);
+                }
+
+                if( item.dataset.source !== "" && item.dataset.source != null && item.dataset.source != undefined){
+                    hashtag = "source="+item.dataset.source;
+                    arr.push(hashtag);
+                }
+
+                if( item.dataset.width === "" ||  item.dataset.width == null ||  item.dataset.width == undefined){
+                    width = "600";
+                } else {
+                    width = item.dataset.width;
+                }
+
+                if( item.dataset.height === "" ||  item.dataset.height == null ||  item.dataset.height == undefined){
+                    height = "255";
+                } else {
+                    height = item.dataset.heigth;
+                }
+
+                var twitterWindow = window.open(arr.join("&"), '', 'height='+ height+',width='+width);
                 if(twitterWindow.focus) { twitterWindow.focus(); }
                 return false;
+            })
+        })
     }
+
+    test(){}
 }
 
-var social = new SocialShare({
-    facebook:{
-        appId:'196979100892953'
-    }
-})
+var social = new SocialShare();
 
 
 //social.test();
+
+//https://www.linkedin.com/shareArticle?mini=true&url=http%3A//google.cl&title=Google&summary=opcional%20decia&source=fuente
